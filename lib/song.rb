0801cced78652f643460354d388ec912cfb4fa19
Jesse Wenger <wengerj@gmail.com>
2021-03-10 14:36:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ class Song
   @@all = []
   attr_accessor :name, :artist, :genre
   
-  def initialize(name, x, y)
+  def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
