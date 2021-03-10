5fa06a042f8378281c0df025add3c75d449fb0c6
Jesse Wenger <wengerj@gmail.com>
2021-03-10 15:59:57 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -15,5 +15,14 @@ class Genre
     Song.all.select{|song| song.genre == self}
   end
   
+  def artists
+    genre_artists = []
+    Song.all.each do |song| 
+      if song.genre == self
+        genre_artists << song.artist
+      end
+    end
+    genre_artists
+  end
   
 end
